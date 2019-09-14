2d229d22acb35fb1ad798216b73395bb31a0427d
Taher <mocrogast_y@hotmail.com>
2019-09-14 09:11:53 +0200

    Add project files.

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Boek naam")]
+        public string Name { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public String Author { get; set; }
+
+    }
+}
